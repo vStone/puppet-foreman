3551101e172c532112497668118f0fb8699a7dee
Jan Vansteenkiste <jan@vstone.eu>
2013-01-09 15:01:27 +0100

    Small workaround to make sure db:migration is not skipped when a error occured.

--- manifests/config/database.pp
+++ manifests/config/database.pp
@@ -7,15 +7,22 @@ class foreman::config::database {
       Package['foreman'],
       File['/etc/foreman/settings.yaml'],
     ],
+    notify  => Exec['foreman_rake_db:migrate_refresh'],
+  }
+
+  exec {'foreman_rake_db:migrate_refresh':
+    command     => 'rm /var/lib/foreman/db.migrated',
+    onlyif      => 'test -f /var/lib/foreman/db.migrated',
+    refreshonly => true,
     notify  => Exec['foreman_rake_db:migrate'],
   }
 
   exec {'foreman_rake_db:migrate':
-    command     => 'bundle exec rake db:migrate',
+    creates     => '/var/lib/foreman/db.migrated',
+    command     => 'bundle exec rake db:migrate && date > /var/lib/foreman/db.migrated',
     environment => ["RAILS_ENV=${foreman::environment}"],
     cwd         => $::foreman::app_root,
     path        => ['/usr/bin','/bin'],
-    refreshonly => true,
     require     => [
       File['/etc/foreman/database.yml'],
     ]
